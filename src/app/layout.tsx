9a19496ebfacc5a858e30b74907dd70479f9e48f
sieun.lee <se.lee@purple.io>
2024-10-01 14:43:01 +0900

    reset css 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "./reset.css";
 
 const godoB = localFont({
   src: "./fonts/GodoB.woff2",
