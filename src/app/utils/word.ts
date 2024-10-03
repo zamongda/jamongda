78bf4a6d3fe856d860de8aa1826afe544cdce01d
sojeongsojeong-park <psj90972@naver.com>
2024-10-03 20:31:01 +0900

    add getWords and addWord api

---
+++ src/app/utils/word.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import { getUser } from './auth';
+import { createClient } from './supabase/create-client';
+
+const supabase = createClient();
+
+export const getWords = async () => {
+	const { user, error: userError } = await getUser();
+	if (userError) {
+		throw new Error(userError.message);
+	}
+	if (!user?.id) {
+		throw new Error('User not found');
+	}
+	const { data, error } = await supabase
+		.from('words')
+		.select('*')
+		.eq('user_id', user?.id);
+
+	if (error) {
+		throw new Error(error.message);
+	}
+
+	return { data: data ? JSON.stringify(data) : '' };
+};
+
+export const addWord = async (
+	words: { ko: string; en: string; category?: string }[]
+) => {
+	const { user, error: userError } = await getUser();
+	const userId = user?.id;
+
+	if (userError) {
+		throw new Error(userError.message);
+	}
+	if (!userId) {
+		throw new Error('User not found');
+	}
+
+	const newWordsArray = words.map((word) => {
+		return { ...word, user_id: userId };
+	});
+
+	const { data, error } = await supabase.from('words').insert(newWordsArray);
+
+	if (error) {
+		throw new Error(error.message);
+	}
+
+	return { data: data ? JSON.stringify(data) : '' };
+};
