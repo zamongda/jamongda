9a8011f570689724a092434524da1ce0d2e3a54d
sojeongsojeong-park <psj90972@naver.com>
2024-12-22 17:48:32 +0900

    회원가입 로직 추가

--- src/app/(pages)/SignUp/components/sign-up.tsx
+++ src/app/(pages)/SignUp/components/sign-up.tsx
@@ -1,22 +1,85 @@
+"use client";
+
 import Button from "@common/button/button";
 import Form from "@common/form/form";
 import Input from "@common/input/input";
+import { useState } from "react";
 import { css, sva } from "@styled-system/css";
+import { useRouter } from "next/navigation";
+import { signUp } from "../../../api/auth";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setChecPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleSignup = async () => {
+    if (password.trim() !== checkPassword.trim()) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    // if (name.trim() === "") {
+    //   alert("이름을 입력해주세요.");
+    //   return;
+    // }
+    if (email.trim() === "") {
+      alert("이메일을 입력해주세요.");
+      return;
+    }
+    if (password.trim() === "") {
+      alert("비밀번호를 입력해주세요.");
+      return;
+    }
+    if (checkPassword.trim() === "") {
+      alert("비밀번호 확인을 입력해주세요.");
+      return;
+    }
+
+    const { data, error } = await signUp(email.trim(), password.trim());
+
+    if (error || !data) {
+      alert("회원가입에 실패했습니다. 잠시 후 다시 시도해주세요.");
+      return;
+    }
+
+    alert("회원가입이 완료되었습니다. 이메일 인증을 완료해주세요.");
+    router.push("/Login");
+  };
   return (
     <div className={signUpStyle.wrapper}>
       <div className={signUpStyle.box}>
         <h3 className={css({ textStyle: "Text-22-M" })}>회원가입</h3>
         <Form>
-          <Input text="이름" name="name" />
-          <Input text="이메일" name="email" />
-          <Input text="비밀번호" name="password" type="password" />
-          <Input text="비밀번호 확인" name="checkPassword" type="password" />
+          {/* <Input text="이름" name="name" /> */}
+          <Input
+            text="이메일"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            text="비밀번호"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            text="비밀번호 확인"
+            name="checkPassword"
+            type="password"
+            value={checkPassword}
+            onChange={(e) => setChecPassword(e.target.value)}
+          />
         </Form>
         <Button
           text="회원가입하기"
           className={css({ mt: "1.875rem !important" })}
+          onClick={handleSignup}
         />
       </div>
     </div>
--- src/app/(pages)/SignUp/page.tsx
+++ src/app/(pages)/SignUp/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@common/header/header";
-
 import SignUp from "./components/sign-up";
 
 const SignUpPage = () => {
